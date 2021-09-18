55663dfc3d0c5aa11b5e1d265995b6d00c26d1cb
David Zapata <zapatadavidt@gmail.com>
2021-09-18 18:37:49 -0500

    modelo inicial usuario

--- socialmediagram/settings.py
+++ socialmediagram/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Installed apps
+    'users',
+    'posts'
 ]
 
 MIDDLEWARE = [
