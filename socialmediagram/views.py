e6f2ef79d37f2d8bdb0929c3f3821a949658aef4
David Zapata <zapatadavidt@gmail.com>
2021-09-18 17:01:23 -0500

    inicial posts views

---
+++ socialmediagram/views.py
@@ -0,0 +1,39 @@
+"""Platzigram views."""
+
+# Django
+from django.http import HttpResponse
+
+# Utilities
+from datetime import datetime
+import json
+
+
+def atletico_nacional(request):
+    """Return a greeting."""
+    return HttpResponse('El Verde! El más grande de colombia!. Hora del servidor donde corre la app {now}'.format(
+        now=datetime.now().strftime('%b %dth, %Y - %H:%M hrs')
+    ))
+
+
+def sort_integers(request):
+    """Return a JSON response with sorted integers."""
+    numbers = [int(i) for i in request.GET['numbers'].split(',')]
+    sorted_ints = sorted(numbers)
+    data = {
+        'status': 'ok',
+        'numbers': sorted_ints,
+        'message': 'Integers sorted successfully.'
+    }
+    return HttpResponse(
+        json.dumps(data, indent=4),
+        content_type='application/json'
+    )
+
+
+def say_hi(request, name, equipo):
+    """Return a greeting."""
+    if str(equipo).lower() != 'nacional':
+        message = 'Sorry {}, you are not allowed here'.format(name)
+    else:
+        message = 'Hello, {}! Welcome to socialmediagram'.format(name)
+    return HttpResponse(message)
