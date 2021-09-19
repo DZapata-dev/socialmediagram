0d259463df8f64f082149da09502a32a8d2dfd5e
David Zapata <zapatadavidt@gmail.com>
2021-09-19 06:58:29 -0500

    Update profile

--- users/views.py
+++ users/views.py
@@ -5,6 +5,47 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+# Exception
+from django.db.utils import IntegrityError
+
+# Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
+# Forms
+from users.forms import ProfileForm
+
+
+def update_profile(request):
+    """Update a user's profile view."""
+    profile = request.user.profile
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            profile.website = data['website']
+            profile.phone_number = data['phone_number']
+            profile.biography = data['biography']
+            profile.picture = data['picture']
+            profile.save()
+
+            return redirect('update_profile')
+
+    else:
+        form = ProfileForm()
+
+    return render(
+        request=request,
+        template_name='users/update_profile.html',
+        context={
+            'profile': profile,
+            'user': request.user,
+            'form': form
+        }
+    )
+
 
 def login_view(request):
     """Login view."""
@@ -34,7 +75,7 @@ def signup(request):
         try:
             user = User.objects.create_user(username=username, password=passwd)
         except IntegrityError:
-            return render(request, 'users/signup.html', {'error': 'Username is already in use'})
+            return render(request, 'users/signup.html', {'error': 'Username is already in user'})
 
         user.first_name = request.POST['first_name']
         user.last_name = request.POST['last_name']
